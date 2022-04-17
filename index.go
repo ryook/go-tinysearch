eb7beb78a31bb9089e0182d0405a8d0328360883
ryook <ryo.kato23@gmail.com>
2022-04-17 18:03:20 +0900

    2.3

--- index.go
+++ index.go
@@ -1,5 +1,7 @@
 package tinysearch
 
+import "container/list"
+
 type Index struct {
 	Dictionary     map[string]PostingList
 	TotalDocsCount int
@@ -24,4 +26,42 @@ type Posting struct {
 
 func NewPosting(docID DocumemtID, positions ...int) *Posting {
 	return &Posting{docID, positions, len(positions)}
+}
+
+type PostingList struct {
+	*list.List
+}
+
+func NewPostingsList(postings ...*Posting) PostingList {
+	l := list.New()
+	for _, posting := range postings {
+		l.PushBack(posting)
+	}
+	return PostingList{l}
+}
+
+func (pl PostingList) add(p *Posting) {
+	pl.PushBack(p)
+}
+
+func (pl PostingList) last() *Posting {
+	e := pl.List.Back()
+	if e == nil {
+		return nil
+	}
+	return e.Value.(*Posting)
+}
+
+// ポスティングをリストに追加
+// ポスティングリストの最後のドキュメントIDを取得して
+// 一致していればポスティングを追加
+// 一致してなければpositionを追加
+func (pl PostingList) Add(new *Posting) {
+	last := pl.last()
+	if last == nil || last.DocID != new.DocID {
+		pl.add(new)
+		return
+	}
+	last.Positions = append(last.Positions, new.Positions...)
+	last.TermFrequency++
 }
