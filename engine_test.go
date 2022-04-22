3b1ff40dc73e24e269a9752bd486100c665890ea
ryook <ryo.kato23@gmail.com>
2022-04-22 17:29:46 +0900

    2.8

---
+++ engine_test.go
@@ -0,0 +1,104 @@
+package tinysearch
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var testDB *sql.DB
+
+func setup() *sql.DB {
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/tinysearch")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = db.Exec(`TRUNCATE TABLE documents`)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := os.RemoveAll("_index_data"); err != nil {
+		log.Fatal(err)
+	}
+	if err := os.Mkdir("_index_data", 0777); err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
+func TestMain(m *testing.M) {
+	testDB = setup()
+	defer testDB.Close()
+	exitCode := m.Run()
+	os.Exit(exitCode)
+}
+
+//インデックス構築のテスト
+func TestCreateIndex(t *testing.T) {
+	engine := NewSearchEngine(testDB)
+
+	type testDoc struct {
+		title string
+		body  string
+	}
+	docs := []testDoc{
+		{"test1", "Do you quarrel, sir?"},
+		{"test2", "No better."},
+		{"test3", "Quarrel sir! no, sir!"},
+	}
+	for _, doc := range docs {
+		r := strings.NewReader(doc.body)
+		if err := engine.AddDocument(doc.title, r); err != nil {
+			t.Fatalf("failed to add document %s: %v", doc.title, err)
+		}
+	}
+
+	// インデックスファイルに書き出して永続化
+	if err := engine.Flush(); err != nil {
+		t.Fatalf("failed to save index to file %V", err)
+
+		type testCase struct {
+			file       string
+			postingStr string
+		}
+
+		testCases := []testCase{
+			{"_index_data/_0.dc", "3"},
+			{"_index_data/better", `[{"DocID": 2, "Positions": [1], "termFrequency": 1}]`},
+			{"_index_data/no", `[{"DocID": 2, "Positions": [0], "termFrequency": 1}, {"DocID": 3, "Positions": [2], "termFrequency": 1}]`},
+			{"_index_data/do", `[{"DocID": 1, "Positions": [0], "termFrequency": 1}]}`},
+			{"_index_data/quarrel", `[{"DocID": 1, "Positions": [2], "termFrequency": 1}, {"DocID": 3, "Positions": [0], "termFrequency": 1}]`},
+			{"_index_data/sir", `[{"DocID": 1, "Positions": [3], "termFrequency": 1}, {"DocID": 3, "Positions": [1,3], "termFrequency": 2}]`},
+			{"_index_data/you", `[{"DocID": 1, "Positions": [1], "termFrequency": 1}]}`},
+		}
+
+		for _, testCase := range testCases {
+			func() {
+				file, err := os.Open(testCase.file)
+				if err != nil {
+					t.Fatalf("failed to load index: %v", err)
+				}
+				defer file.Close()
+				b, err := ioutil.ReadAll(file)
+				if err != nil {
+					t.Fatalf("failed to load index: %v", err)
+				}
+				got := string(b)
+				var buf bytes.Buffer
+				_ = json.Compact(&buf, []byte(testCase.postingStr))
+				want := buf.String()
+				if got != want {
+					t.Errorf("got : %v\nwant: %v\n", got, want)
+				}
+			}()
+		}
+	}
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ryook/tinysearch
 go 1.18
 
 require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	golang.org/x/mod v0.6.0-dev.0.20220106191415-9b9b3d81d5e3 // indirect
 	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	golang.org/x/tools v0.1.10 // indirect
