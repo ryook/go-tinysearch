50b677335072728b498688a087f737670a9982f7
ryook <ryo.kato23@gmail.com>
2022-04-24 16:24:58 +0900

    3.4

--- searcher.go
+++ searcher.go
@@ -2,6 +2,7 @@ package tinysearch
 
 import (
 	"fmt"
+	"sort"
 )
 
 type TopDocs struct {
@@ -21,4 +22,31 @@ type ScoreDoc struct {
 
 func (d ScoreDoc) String() string {
 	return fmt.Sprintf("docId: %v, Score: %v", d.docID, d.score)
+}
+
+type Searcher struct {
+	indexReader *IndexReader
+	cursors     []*Cursor
+}
+
+func NewSearcher(path string) *Searcher {
+	return &Searcher{indexReader: NewIndexReader(path)}
+}
+
+func (s *Searcher) SearchTopK(query []string, k int) *TopDocs {
+	results := s.search(query)
+
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].score > results[j].score
+	})
+
+	total := len(results)
+	if len(results) > k {
+		results = results[:k]
+	}
+
+	return &TopDocs{
+		totalHits: total,
+		scoreDocs: results,
+	}
 }
